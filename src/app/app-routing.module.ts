5a5b11d5b29b8e6668d0ab68fabff90583a6ab1d
= <amc1140@aol.com>
2022-06-01 22:23:20 -0700

    Creating a flexbox container for a home page header that has an image and title text.
    Plus, other minor upgrades to the navbar and section header bar (about me) to better align with the header style.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AboutComponent}, // Set About as the default/home page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from "./nav/nav.component";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
