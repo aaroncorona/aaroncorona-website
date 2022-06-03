e4f1d1f1196b48edee8ba0914e20d4c2740f4e56
= <amc1140@aol.com>
2022-06-02 22:30:11 -0700

    Adding a new page for Work Experience, updating routing rules accordingly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
+import {ExpComponent} from "./exp/exp.component";
 
 const routes: Routes = [
   {path: '', component: AboutComponent}, // Set About as the default/home page
+  {path: 'exp', component: ExpComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from "./nav/nav.component";
 import { AboutComponent } from './about/about.component';
+import { ExpComponent } from './exp/exp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AboutComponent
+    AboutComponent,
+    ExpComponent
   ],
   imports: [
     BrowserModule,
