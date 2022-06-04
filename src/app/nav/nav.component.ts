fd8174831266664993530e315ba671fd3c2ebde9
= <amc1140@aol.com>
2022-06-04 16:46:43 -0700

    Adding more inner card container classes for better scalabililty to other card types and overall code consistency.
    Also, completing the template for a project card.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  public isActive:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
